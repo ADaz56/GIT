5a1ef20f67dc8c23a46cc33e09edace95d46f2c2
Utilisateur 1 <dazin.a@stjolorient.fr>
2025-01-13 16:21:22 +0100

    coucou

--- C/math.c
+++ C/math.c
@@ -1,21 +1,28 @@
 /****** Module manipulant des fonctions mathématiques **********/
 #include "math.h"
-
 /****************** Recherche du maximum ***********************/
 float maximum (float n1, float n2 )
 {
-   return 0;
+   if (n1>n2){
+	   return n1;
+	   }
+	else {
+		return n2;
+	}
 }
-
 /****************** Recherche du minimum ***********************/
 float minimum (float n1, float n2 )
 {
-   return 0;
+	if (n1>n2){
+	   return n2;
+	   };
+	else {
+		return n1;
+	};
 }
-
 /******************* Addition de nombres ***********************/
 float addition (float n1, float n2 )
 {
-   return 0;
+	int nb = n1 + n2;
+	return nb;
 }
-
